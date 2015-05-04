f8a7d3a69ac941efd28936b2c72c9506e8b43a7d
wsn <bw2682@gmail.com>
2015-05-04 15:54:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wsn/myapplication/Uzytkownik.java
@@ -0,0 +1,14 @@
+package com.example.wsn.myapplication;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by wsn on 2015-05-04.
+ */
+public class Uzytkownik extends MainActivity
+{
+    EditText login;
+    EditText password;
+    Button b1;
+}
