ce53305ff64207a06a7dcbeb5170dccd605d9fdb
wsn <bw2682@gmail.com>
2015-05-04 16:53:36 +0200

    lol

--- app/src/main/java/com/example/wsn/myapplication/Uzytkownik.java
+++ app/src/main/java/com/example/wsn/myapplication/Uzytkownik.java
@@ -6,9 +6,8 @@ import android.widget.EditText;
 /**
  * Created by wsn on 2015-05-04.
  */
-public class Uzytkownik extends MainActivity
-{
+public class Uzytkownik extends MainActivity {
     EditText login;
     EditText password;
-    Button b1;
+    Button b1;  //chuj
 }
